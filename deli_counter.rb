0f9a833c8a83f34ef831f2eb765152a16bb52b3b
Matthew Ream <matthewtimothyream@gmail.com>
2019-08-22 23:07:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+def line(katz_deli)
+  sentence = "The line is currently:"
+  if katz_deli.count == 0
+     puts "The line is currently empty."
+ else 
+   katz_deli.each_with_index do |name, index|
+     sentence << " #{index + 1}. #{name}"
+    end
+  puts sentence
+  end
+end
+
+def take_a_number(katz_deli, name)
+  if katz_deli.count == 0
+    katz_deli << name
+    puts "Welcome, #{name}. You are number 1 in line."
+  else katz_deli << name
+    puts "Welcome, #{name}. You are #{katz_deli.length} person in line."
+end
